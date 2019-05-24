acef3f4d90b9f29b94321add50520a63ff35edee
gayathriMurugavel <43488091+gayathriMurugavel@users.noreply.github.com>
2019-05-24 15:10:10 +0530

    Add files via upload

---
+++ kthSmallest.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class kthSmallest{
+public static void main(String[] args){
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+			arr[i]=in.nextInt();
+		Arrays.sort(arr);
+		System.out.print(arr[0]);
+	}
+}
