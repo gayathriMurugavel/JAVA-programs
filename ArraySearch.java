6984e2926d5a4e0e6d6b969711185241e2e45ca8
gayathriMurugavel <43488091+gayathriMurugavel@users.noreply.github.com>
2019-05-29 09:17:09 +0530

    Update ArraySearch.java

--- ArraySearch.java
+++ ArraySearch.java
@@ -4,8 +4,8 @@ public class ArraySearch{
 		Scanner in=new Scanner(System.in);
 		int arr[][]={{1,2,3,4},{5,6,7,8},{9,10,11,17},{18,19,20,21}};
 		int n=in.nextInt();
-		for(int i=arr.length-1;i>=0;i--){
-			for(int j=0;j<arr.length;j++){
+		for(int i=0;i<arr.length;i++){
+			for(int j=arr.lengt-1;j>=0;j--){
 				if(n<arr[i][j])
 					break;
 				else
